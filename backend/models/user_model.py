7a9acb1239ad2d3ead6118bbfe2872d84f7c44d1
roxhit <rohitsingh692004@gmail.com>
2024-09-27 21:31:26 +0530

    modified:   backend/main.py 	modified:   backend/models/user_model.py 	modified:   backend/requirements.txt 	modified:   backend/routes/user_route.py 	modified:   tailwind.config.js

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel
-
+from pydantic import BaseModel,Field
+from typing import Optional
+from datetime import datetime
 class UserRegister(BaseModel):
     first_name : str
     last_name:str
@@ -10,3 +11,14 @@ class UserRegister(BaseModel):
 class UserLogin(BaseModel):
     email:str
     password:str
+
+class UserProfile(BaseModel):
+    height: int
+    weight: int
+    addiction_type: str  # E.g., Alcohol, Smoking, Gaming, etc.
+    time_spent: float = Field(..., description="Time spent on the addiction per day in hours")
+    quantity_consumed: float = Field(..., description="Quantity of the substance or activity per day")
+    recovery_goal: Optional[str] = Field(None, description="User's goal for addiction recovery")
+    start_date: Optional[datetime] = Field(None, description="Date when the user started recovery")
+    end_date: Optional[datetime] = Field(None, description="Date when the user aims to fully recover")
+    
