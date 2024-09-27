7195bc40153c2fdb7ec74b1840eee0187659a693
roxhit <rohitsingh692004@gmail.com>
2024-09-27 11:45:21 +0530

    Added Register Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import Services from "./components/Services";
 import Team from "./components/Team";
+import Register from "./components/Register";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <HeroSection />
-      <About />
-      <Services />
-      <Team />
-      <Contact />
-    </div>
+    <Routes>
+      <Route path="/" element={<Register />} />
+      <Route
+        path="/dashboard"
+        element={
+          <>
+            <Header />
+            <HeroSection />
+            <About />
+            <Services />
+            <Team />
+            <Contact />
+          </>
+        }
+      />
+    </Routes>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
+import App from "./App";
+import "./index.css"; // Make sure the path is correct
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>,
+  document.getElementById("root") // Assuming your index.html has a div with id="root"
+);
